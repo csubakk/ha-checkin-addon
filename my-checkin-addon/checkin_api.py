460643bd79a541fe2897a15365c8ecf8434144fd
csubakk <79394162+csubakk@users.noreply.github.com>
2025-06-15 14:59:35 +0300

    Update checkin_api.py

--- my-checkin-addon/checkin_api.py
+++ my-checkin-addon/checkin_api.py
@@ -6,6 +6,7 @@ import requests
 from fastapi import FastAPI, HTTPException, Form
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+from checkin_meta_api import router as meta_router
 
 DB_PATH = "/config/guestbook.db"
 SCRIPT_PATH = "/config/scripts/send_access_link.py"
@@ -13,6 +14,8 @@ DOOR_MAP_PATH = "/config/guest_door_map.yaml"
 HA_URL = "http://homeassistant.local:8123/api/services"
 
 app = FastAPI()
+
+app.include_router(meta_router)
 
 app.add_middleware(
     CORSMiddleware,
