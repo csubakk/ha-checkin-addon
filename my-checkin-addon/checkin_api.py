b3cffa65cf4c9586644df5eb82dcfb6dfb798f2d
csubakk <79394162+csubakk@users.noreply.github.com>
2025-06-16 20:07:23 +0300

    Update checkin_api.py

--- my-checkin-addon/checkin_api.py
+++ my-checkin-addon/checkin_api.py
@@ -22,7 +22,7 @@ from PIL import Image
 from io import BytesIO
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfbase import pdfmetrics
-pdfmetrics.registerFont(TTFont('DejaVuSans', '/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf'))
+pdfmetrics.registerFont(TTFont('DejaVuSans', '/config/DejaVuSans.ttf'))
 
 def generate_guest_pdf(data: dict, path: str):
     c = canvas.Canvas(path, pagesize=A4)
