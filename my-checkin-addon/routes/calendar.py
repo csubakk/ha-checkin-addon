6b4e7ba253ee07a8bd383499774ecd5aa2dd6754
csubakk <79394162+csubakk@users.noreply.github.com>
2025-06-19 17:47:00 +0300

    Update calendar.py

--- my-checkin-addon/routes/calendar.py
+++ my-checkin-addon/routes/calendar.py
@@ -65,6 +65,7 @@ def calendar_page(request: Request):
     <html lang="hu">
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Foglalási naptár – 1 szoba</title>
         
         <style>
