2319311ead307a11552c9f281e6e0d7a68727e69
csubakk <79394162+csubakk@users.noreply.github.com>
2025-06-20 12:51:21 +0300

    Update checkin_api.py

--- my-checkin-addon/checkin_api.py
+++ my-checkin-addon/checkin_api.py
@@ -12,7 +12,7 @@ from reportlab.lib.pagesizes import A4
 from reportlab.pdfgen import canvas
 from reportlab.lib.colors import HexColor
 from checkin_meta_api import router as meta_router
-from routes import calendar, booking_editor
+from routes import calendar
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
