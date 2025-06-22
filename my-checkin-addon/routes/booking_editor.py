374afb6c3d3183a216236dce80e3401a2e62fdc5
csubakk <79394162+csubakk@users.noreply.github.com>
2025-06-22 22:09:05 +0300

    Update booking_editor.py

--- my-checkin-addon/routes/booking_editor.py
+++ my-checkin-addon/routes/booking_editor.py
@@ -299,7 +299,7 @@ async def save_booking(
             f.write(f"access_token: {repr(access_token)}\n")
             values = (
                 guest_first_name, guest_last_name, None, None, None,
-                None, None, None, None, None, None,  None, # signature is the last of these
+                None, None, None, None, None, None, 
                 checkin_time, checkout_time, guest_count, notes, None,
                 0, now, now, guest_email, guest_phone,
                 guest_house_id, access_token, created_by, None,
@@ -319,7 +319,7 @@ async def save_booking(
             ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
         """, (
             guest_first_name, guest_last_name, None, None, None,
-            None, None, None, None, None, None,  None, # signature is the last of these
+            None, None, None, None, None, None, 
             checkin_time, checkout_time, guest_count, notes, None,
             0, now, now, guest_email, guest_phone,
             guest_house_id, access_token, created_by, None,
