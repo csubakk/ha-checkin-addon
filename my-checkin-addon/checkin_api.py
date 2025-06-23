d60b1e371c6f5b4d8908d4cf4bb3ef44000c4092
csubakk <79394162+csubakk@users.noreply.github.com>
2025-06-23 18:31:19 +0300

    Update checkin_api.py

--- my-checkin-addon/checkin_api.py
+++ my-checkin-addon/checkin_api.py
@@ -14,7 +14,7 @@ from reportlab.lib.colors import HexColor
 from checkin_meta_api import router as meta_router
 from services import notifications as email_notifications
 from routes import calendar, booking_editor, notifications as api_notifications
-from routes import token_api as token_router
+from routes.token_api import token_api
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -70,7 +70,7 @@ app.include_router(meta_router)
 app.include_router(calendar.router)
 app.include_router(booking_editor.router)
 app.include_router(api_notifications.router)
-app.include_router(token_router)
+app.include_router(token_api)
 
 app.add_middleware(
     CORSMiddleware,
