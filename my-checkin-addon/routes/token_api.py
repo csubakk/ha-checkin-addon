3d701e74b7ede3373637296270e78a8b99413dde
csubakk <79394162+csubakk@users.noreply.github.com>
2025-06-23 19:01:25 +0300

    Update token_api.py

--- my-checkin-addon/routes/token_api.py
+++ my-checkin-addon/routes/token_api.py
@@ -1,11 +1,11 @@
-from fastapi import APIRouter
-from datetime import datetime
-import hashlib
+# from fastapi import APIRouter
+# from datetime import datetime
+# import hashlib
 
-token_api = APIRouter()
+# token_api = APIRouter()
 
-@token_api.post("/generate_token")
-def generate_token():
-    timestamp = str(datetime.now().timestamp()).encode("utf-8")
-    token = hashlib.sha256(timestamp).hexdigest()[:32]
-    return {"token": token}
+# @token_api.post("/generate_token")
+# def generate_token():
+#     timestamp = str(datetime.now().timestamp()).encode("utf-8")
+#     token = hashlib.sha256(timestamp).hexdigest()[:32]
+#     return {"token": token}
